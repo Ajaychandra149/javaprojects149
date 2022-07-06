5a45473fa9b4060d800f74b87fe1abebb4518f36
ajay <ajaychandra149@gmail.com>
2022-07-06 13:13:35 +0530

    adding QuadEq.java file

---
+++ QuadEq.java
@@ -0,0 +1,12 @@
+package assnmnt3;
+
+public class QuadEq {
+	int x,y,z;
+	public QuadEq() {
+		System.out.println("QuadEq="+(x+y+z)*x);
+	}
+	public void Qq1() {
+		System.out.println("QuadEq="+(x+y+z)*x);
+	}
+
+}
