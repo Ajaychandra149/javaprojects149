0dc83010d0758ab47c4414e3521589a0b2ab4b08
ajay <ajaychandra149@gmail.com>
2022-07-06 14:47:30 +0530

    adding A.java file

---
+++ A.java
@@ -0,0 +1,6 @@
+package inheritance;
+
+public class A {
+	public int x=20;
+	public double pi=3.14;
+}
---
+++ B.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class B extends A {
+	int radius=10;
+    int x=30;
+	public void area() {
+
+		System.out.println("area="+(pi*radius*radius));
+	}
+}
